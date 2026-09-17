87768b41ef9cd56bd4595b00d7b6813df8225b2b
Afrin-banu-A <afrinbanu.06@outlook.com>
2026-09-17 20:36:34 +0530

    Time: 0 ms (100%), Space: 21.1 MB (44.62%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        ans=0
+        for num in nums:
+            ans=ans^num
+
+        return ans
+
+            
+        
